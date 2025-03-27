7e3bb411c140dba9c04cbc73a662be04e3c7b862
Tobias Federico Alvarez <138098809+tobiasalvarez@users.noreply.github.com>
2025-03-27 19:15:24 -0300

    .

---
+++ proximaOferta/src/test/java/com/example/proximaOferta/ProximaOfertaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proximaOferta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProximaOfertaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
