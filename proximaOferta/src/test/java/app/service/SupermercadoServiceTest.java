a10560db7cdb3c0b1f9e697b7a819652ff64d969
Tobias Federico Alvarez <138098809+tobiasalvarez@users.noreply.github.com>
2025-04-30 11:03:45 -0300

    testes

--- proximaOferta/src/test/java/app/service/SupermercadoServiceTest.java
+++ proximaOferta/src/test/java/app/service/SupermercadoServiceTest.java
@@ -1,12 +1,16 @@
 package app.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import app.Entity.Supermercado;
+import app.Repository.SupermercadoRepository;
 import app.Service.SupermercadoService;
 
 @SpringBootTest
@@ -15,14 +19,23 @@ public class SupermercadoServiceTest {
 	@Autowired
 	SupermercadoService supermercadoService;
 	
+	@Mock
+	SupermercadoRepository supermercadoRepository;
+	
 	@Test
+	@DisplayName("Teste Unitario com Mockito")
 	void testCreate() {
 		Supermercado supermercado = new Supermercado();
 		supermercado.setId(1);
 		supermercado.setNome("aaaa");
 		supermercado.setEmail("abc@gmail.com");
 		supermercado.setRua("aaaaaa");
+		
+		when(supermercadoRepository.save(supermercado)).thenReturn(supermercado);
+		
 		Supermercado retorno = this.supermercadoService.save(supermercado);
+		
+		
 		assertEquals(1, retorno.getId());
 		assertEquals("aaaa", retorno.getNome());
 		assertEquals("abc@gmail.com", retorno.getEmail());
